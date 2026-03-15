7240408d024b34b69202b9fc79871fbe077d96e5
Bladestar2105 <55372949+Bladestar2105@users.noreply.github.com>
2026-03-15 01:34:56 +0000

    ⚡ Bolt: Use isolate for XMLTV parsing

--- lib/services/xmltv_parser.dart
+++ lib/services/xmltv_parser.dart
@@ -1,7 +1,76 @@
 import 'package:xml/xml.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 import '../models/iptv.dart';
 import 'package:intl/intl.dart';
+
+// Top-level function for isolate
+List<ParsedProgram> _parseXmltv(String xmlBody) {
+  final document = XmlDocument.parse(xmlBody);
+  final programmes = document.findAllElements('programme');
+
+  List<ParsedProgram> result = [];
+
+  for (var p in programmes) {
+    final channelId = p.getAttribute('channel');
+    if (channelId == null) continue;
+
+    final startStr = p.getAttribute('start');
+    final stopStr = p.getAttribute('stop');
+
+    if (startStr == null || stopStr == null) continue;
+
+    final startMs = _parseXmltvDate(startStr);
+    final stopMs = _parseXmltvDate(stopStr);
+
+    if (startMs == null || stopMs == null) continue;
+
+    final titleNode = p.findElements('title').firstOrNull;
+    final descNode = p.findElements('desc').firstOrNull;
+
+    result.add(ParsedProgram(
+      start: startMs ~/ 1000,
+      end: stopMs ~/ 1000,
+      title_raw: titleNode?.innerText ?? 'Unknown Title',
+      description_raw: descNode?.innerText ?? '',
+      start_formatted: channelId, // temporarily store channelId in start_formatted to group later
+    ));
+  }
+  return result;
+}
+
+int? _parseXmltvDate(String dateString) {
+  if (dateString.length < 14) return null;
+
+  // Format: YYYYMMDDHHmmss +HHMM
+  try {
+    final year = int.parse(dateString.substring(0, 4));
+    final month = int.parse(dateString.substring(4, 6));
+    final day = int.parse(dateString.substring(6, 8));
+    final hour = int.parse(dateString.substring(8, 10));
+    final min = int.parse(dateString.substring(10, 12));
+    final sec = int.parse(dateString.substring(12, 14));
+
+    DateTime date = DateTime.utc(year, month, day, hour, min, sec);
+
+    if (dateString.length >= 19) {
+      final offsetSign = dateString.substring(15, 16);
+      final offsetHour = int.parse(dateString.substring(16, 18));
+      final offsetMin = int.parse(dateString.substring(18, 20));
+
+      int totalOffsetMinutes = (offsetHour * 60) + offsetMin;
+      if (offsetSign == '+') {
+        date = date.subtract(Duration(minutes: totalOffsetMinutes));
+      } else if (offsetSign == '-') {
+        date = date.add(Duration(minutes: totalOffsetMinutes));
+      }
+    }
+
+    return date.millisecondsSinceEpoch;
+  } catch (e) {
+    return null;
+  }
+}
 
 class XMLTVParser {
   final String url;
@@ -12,40 +81,11 @@ class XMLTVParser {
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
-        final document = XmlDocument.parse(response.body);
-        final programmes = document.findAllElements('programme');
-
-        List<ParsedProgram> result = [];
-
-        for (var p in programmes) {
-          final channelId = p.getAttribute('channel');
-          if (channelId == null) continue;
-
-          final startStr = p.getAttribute('start');
-          final stopStr = p.getAttribute('stop');
-
-          if (startStr == null || stopStr == null) continue;
-
-          final startMs = parseXmltvDate(startStr);
-          final stopMs = parseXmltvDate(stopStr);
-
-          if (startMs == null || stopMs == null) continue;
-
-          final titleNode = p.findElements('title').firstOrNull;
-          final descNode = p.findElements('desc').firstOrNull;
-
-          result.add(ParsedProgram(
-            start: startMs ~/ 1000,
-            end: stopMs ~/ 1000,
-            title_raw: titleNode?.innerText ?? 'Unknown Title',
-            description_raw: descNode?.innerText ?? '',
-            start_formatted: channelId, // temporarily store channelId in start_formatted to group later
-          ));
-        }
-        return result;
+        // Offload heavy XML parsing to a background isolate
+        return await compute(_parseXmltv, response.body);
       }
     } catch (e) {
-      print('XMLTV Parse Error: $e');
+      debugPrint('XMLTV Parse Error: $e');
     }
     return [];
   }
@@ -75,38 +115,7 @@ class XMLTVParser {
     return grouped;
   }
 
-  int? parseXmltvDate(String dateString) {
-    if (dateString.length < 14) return null;
-
-    // Format: YYYYMMDDHHmmss +HHMM
-    try {
-      final year = int.parse(dateString.substring(0, 4));
-      final month = int.parse(dateString.substring(4, 6));
-      final day = int.parse(dateString.substring(6, 8));
-      final hour = int.parse(dateString.substring(8, 10));
-      final min = int.parse(dateString.substring(10, 12));
-      final sec = int.parse(dateString.substring(12, 14));
-
-      DateTime date = DateTime.utc(year, month, day, hour, min, sec);
-
-      if (dateString.length >= 19) {
-        final offsetSign = dateString.substring(15, 16);
-        final offsetHour = int.parse(dateString.substring(16, 18));
-        final offsetMin = int.parse(dateString.substring(18, 20));
-
-        int totalOffsetMinutes = (offsetHour * 60) + offsetMin;
-        if (offsetSign == '+') {
-          date = date.subtract(Duration(minutes: totalOffsetMinutes));
-        } else if (offsetSign == '-') {
-          date = date.add(Duration(minutes: totalOffsetMinutes));
-        }
-      }
-
-      return date.millisecondsSinceEpoch;
-    } catch (e) {
-      return null;
-    }
-  }
+  int? parseXmltvDate(String dateString) => _parseXmltvDate(dateString);
 
   String formatProgramTime(int timestamp) {
     final date = DateTime.fromMillisecondsSinceEpoch(timestamp);
