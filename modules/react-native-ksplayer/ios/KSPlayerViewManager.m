1538bcf7d1f646f6dae0bc361b5e6a3d6508f3dd
root <root@172.21.161.45>
2026-03-29 13:21:58 +0000

    fix: correct RCT_EXTERN_MODULE syntax for KSPlayerViewManager
    The @interface RCT_EXTERN_MODULE(...) syntax is required for Swift
    ViewManagers bridged to Objective-C. The previous incorrect syntax
    caused compilation errors about missing type specifiers.
    
    Fixes:
    - error: type specifier missing, defaults to 'int'
    - error: expected ';' after top level declarator
    - error: prefix attribute must be followed by an interface

--- modules/react-native-ksplayer/ios/KSPlayerViewManager.m
+++ modules/react-native-ksplayer/ios/KSPlayerViewManager.m
@@ -1,3 +1,4 @@
+#import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
 
 // ---------------------------------------------------------------------------
@@ -7,7 +8,7 @@
 // can create and configure KSPlayerView instances from JavaScript.
 // ---------------------------------------------------------------------------
 
-RCT_EXTERN_MODULE(KSPlayerViewManager, RCTViewManager)
+@interface RCT_EXTERN_MODULE(KSPlayerViewManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(streamUrl, NSString)
 RCT_EXPORT_VIEW_PROPERTY(paused, BOOL)
@@ -16,3 +17,5 @@ RCT_EXPORT_VIEW_PROPERTY(onVideoLoad, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onVideoError, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onProgress, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onPlayerState, RCTBubblingEventBlock)
+
+@end
