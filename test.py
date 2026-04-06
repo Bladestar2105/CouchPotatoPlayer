005cedb50a8a7e090a84f095c40743230d794fea
Bladestar2105 <55372949+Bladestar2105@users.noreply.github.com>
2026-04-06 15:11:51 +0000

    🎨 Palette: Apple TV UX and Bug Fixes

--- test.py
+++
@@ -1,2 +0,0 @@
-# The user issue states: "Zeitleiste Uhrzeiten immer volle halbe und ganze stunden anzeigen und nicht zum beispiel 15:31 Uhr."
-# The bug must be in `generateTimeMarkers` or headers push.
--- test2.py
+++
@@ -1,8 +0,0 @@
-# The focus logic in ChannelList seems correct:
-# route.params.focusChannelId triggers setFocusedChannelId
-# ChannelRow sets hasTVPreferredFocus={isFocused}
-# Is there a bug where focusedChannelId is set but doesn't get focus because firstCategoryRef steals it?
-# In MainLayout, when navigating back, `route.params.returnTab === 'channels'` triggers:
-# `contentRef.current?.focusFirstItem();`
-# But focusFirstItem explicitly checks: `if (viewMode === 'epg' || restoreFocusOnSelectedChannel) return;`
-# So if restoreFocusOnSelectedChannel is true, it doesn't steal focus!
